21eec76b41034ada01428b545e8513d9dd9ee8e9
Critma <69364247+Critma@users.noreply.github.com>
2023-01-30 18:55:45 +0500

    Vector,

---
+++ ReadOnlyVector/ReadOnlyVector/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlyVectorTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var c = new ReadOnlyVector(1, 2).WithX(3);
+            Console.WriteLine(c);
+        }
+    }
+}
